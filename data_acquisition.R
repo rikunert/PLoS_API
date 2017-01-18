639755449c0cbeb10cbf961bf28201f701fe084e
Richard Kunert <Richard Kunert>
2017-01-18 15:02:54 +0100

    debugged data acquisition script

--- data_acquisition.R
+++ data_acquisition.R
@@ -16,7 +16,8 @@ library(rplos)
 # set global variables
 
 #How many articles to sample?
-sample_size = 15 * 10^3#type 'all' to sample all available articles
+sample_size = 0.5 * 1e3
+  #15 * 10^3#type 'all' to sample all available articles
 
 #find out how many articles we should look for
 dois = searchplos(q = "*:*",#search for potentially all articles
@@ -34,11 +35,30 @@ dois = searchplos(q = "*:*",#search for potentially all articles
                   start = 0,
                   limit = sample_size)#how many articles to look for (irrelevant here as interest is in meta field)
 
+#save doi s for reference
+save(dois,       
+     file=sprintf("PLOSdata_dois_%s.RData", Sys.Date()))
+
 ###################################################################################################
 # custom functions
 
 #custom function for extracting ALM of a given article defined by doi
 ALM_extr <- function(doi){
+ #input: doi identifying an article in the Public Library of Science
+ #output: list with info about this article
+  
+  #custom function for avoiding numerical null entries, replacing them with NaN
+  NulltoNaN <- function(numInput){
+    #input: a numerical entry  
+    #output: the same as input if input not null and if input numerical, otherwise NaN
+    
+    if (is.numeric(numInput) && length(numInput) > 0){
+      numOutput = numInput
+    } else{
+      numOutput = NaN
+    }
+    return(numOutput)
+  }
   
   print(sprintf('Processing article with doi: %s', doi))#for debugging purposes and user feedback purposes only
   #tic = proc.time()[3]#start a timer (for debugging and efficiency purposes)
@@ -56,25 +76,30 @@ ALM_extr <- function(doi){
     
     article$id = tmp$data$id[1]#the doi of the article
     article$publication_date = tmp$data$publication_date[1] # date of publication
+    
     article$review_time = NaN #fill review time with NA in case following try-statement is not successful
     try(article$review_time <- difftime(as.Date(tmp$data$publication_date[1]), as.Date(tmp$data$received_date[1]),
                                         units = "days"),#time between reception of article and publication of article (in days)
         silent = T)#many received dates are unknown or badly formatted, so error handling is crucial
-    article$publication_year = strtoi(substring(tmp$data$publication_date[1], 1, 4)) # year of publication (integer)
     
-    article$authors = tmp$data$author[1]#authors
-    tmp_authors_split = strsplit(article$authors, split = '; ')[[1]]
+    #article$publication_year = strtoi(substring(tmp$data$publication_date[1], 1, 4)) # year of publication (integer)
+    #authors are not acquired in order to save memory
+    
+    #article$authors = tmp$data$author[1]#authors
+    #authors are not acquired in order to save memory
+    
+    tmp_authors_split = strsplit(tmp$data$author[1], split = '; ')[[1]]
     article$author_count = length(tmp_authors_split)
     
-    article$journal = tmp$data$journal[1]#journal name
+    #sometimes the journal name is somehow missing
+    if (is.null(tmp$data$journal[1])){
+      article$journal = NA#just in case the following journal field is null  
+    } else {
+      article$journal = tmp$data$journal[1]#journal name
+    }
     
     #for some reason the page count is sometimes missing
-    tmp_page_count = strtoi(tmp$data$pagecount[1])
-    if (is.numeric(tmp_page_count) && length(tmp_page_count) > 0){
-      article$page_count = tmp_page_count
-    } else{
-      article$page_count = NaN
-    }
+    article$page_count = NulltoNaN(strtoi(tmp$data$pagecount[1]))
     
     #Sometimes the references are not included in the API-output. Wrap code in try to avoid error.
     article$reference_count = NaN
@@ -84,30 +109,19 @@ ALM_extr <- function(doi){
     
     article$subject = tmp$data$subject_level_1[1]#for list of subjects see https://github.com/PLOS/plos-thesaurus/blob/master/plosthes.2016-3.full.xlsx
     article$subject_count = length(strsplit(article$subject, split = '; ')[[1]])
-    article$title = tmp$data$title[1]#title
+    
+    #article$title = tmp$data$title[1]#title
+    #title not acquired in order to save memory
     
     #get article level metric info
     tmp = alm_ids(doi = doi, info = 'totals')
     
-    #tweets are sometimes missing
-    tmp_tweets = tmp$data$total[11]
-    if (is.numeric(tmp_tweets) && length(tmp_tweets) > 0){
-      article$tweets = tmp_tweets
-    } else{
-      article$tweets = NaN
-    }
-    
-    #FB engagements (readers, comments, likes) are sometimes missing
-    tmp_facebook = tmp$data$total[9]
-    if (is.numeric(tmp_facebook) && length(tmp_facebook) > 0){
-      article$facebook = tmp_facebook
-    } else{
-      article$facebook = NaN
-    }
-    
-    article$scopus_cites= tmp$data$total[5]#citations (crossref, and scopus)
-    article$reads = tmp$data$total[6] + tmp$data$total[8]#reads/views (sum of counter and pmc)
-    article$saves = tmp$data$total[10] + tmp$data$total[1]#saves/downloads (mendeley, and citeulike)
+    #some ALM are sometimes missing, therefore wrap in NulltoNaN function
+    article$tweets = NulltoNaN(tmp$data$total[11])
+    article$facebook = NulltoNaN(tmp$data$total[9])
+    article$scopus_cites= NulltoNaN(tmp$data$total[5])#citations (crossref, and scopus)
+    article$reads = NulltoNaN(tmp$data$total[6]) + NulltoNaN(tmp$data$total[8])#reads/views (sum of counter and pmc)
+    article$saves = NulltoNaN(tmp$data$total[10]) + NulltoNaN(tmp$data$total[1])#saves/downloads (mendeley, and citeulike)
     
   }
   
@@ -124,7 +138,7 @@ ALM_extr <- function(doi){
 #Predict when code will have run (very rough estimate)
 print(sprintf('%d articles to process (%1.2f per cent of total), ending approximately at %s.', sample_size,
               round((sample_size/article_count) * 100, digits = 2),
-              as.character(Sys.time() + 1.15 * sample_size)))#I assume a per article processing time of 1.15 sec 
+              as.character(Sys.time() + 1.25 * sample_size)))#I assume a per article processing time of 1.15 sec 
 
 tic_global = proc.time()[3]#start a timer (for debugging and efficiency purposes)
 
@@ -138,20 +152,16 @@ print(sprintf('Actual duration: %1.1f sec, or %1.2f sec per article', toc_global
 
 PLOS_data = do.call(rbind, PLOS_data)#create matrix from list
 
-#coerce the different data types into the right classes
-NumCols = c(3, 4, 6, 8, 9, 11, 13 : 17)#the numerical columns in the final data frame
-PLOS_data_num = apply(PLOS_data[,NumCols], 2, unlist)#numerical columns are now in a numerical matrix
+#coerce the different data types into the right classes and put into data frame
+NumCols = c(3, 4, 6, 7, 9 : 14)#review_time, author_count, page_count, reference_count, subject_count, tweets, fecebook, scopus_cites, reads, saves
+TextCols = c(1, 8)#id, subject
+FactCols = c(5)#journal
+DateCols = c(2)#publication_date
 
-TextCols = c(1, 5, 7, 10, 12)#the string  columns in the final data frame
-PLOS_data_str = apply(PLOS_data[,TextCols], 2, unlist)#string columns are now in a string matrix
-
-DateCols = c(2)
-PLOS_data_date = as.Date(unlist(PLOS_data[,DateCols]))#coerce dates into dates
-
-#put all data into 1 data frame
-PLOS_data = as.data.frame(c(as.data.frame(PLOS_data_num),
-                             as.data.frame(PLOS_data_str, stringsAsFactors=FALSE),
-                             data.frame(publication_date = PLOS_data_date)))
+PLOS_data = as.data.frame(c(as.data.frame(apply(PLOS_data[,NumCols], 2, unlist)),
+                            as.data.frame(apply(PLOS_data[,TextCols], 2, unlist), stringsAsFactors=FALSE),
+                            data.frame(journal = as.factor(unlist(PLOS_data[,FactCols]))),
+                            data.frame(publication_date = as.Date(unlist(PLOS_data[,DateCols])))))
 
 ###################################################################################################
 # save data
